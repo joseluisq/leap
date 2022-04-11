5b0cf0068113cbbcee99bc25c5f8adf462e602d4
Jose Quintana <joseluisquintana20@gmail.com>
2022-04-11 23:19:07 +0200

    chore: database class

--- src/Controller.php
+++ src/Controller.php
@@ -6,6 +6,7 @@ namespace Leap;
 
 use Twig\Environment as TwigEnvironment;
 use Twig\Loader\FilesystemLoader as TwigFilesystemLoader;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 /** `Leap` base controller class to be extended by other controllers. */
 class Controller
@@ -14,23 +15,30 @@ class Controller
     public array $route_data = [];
 
     /** @param ?TwigEnvironment View */
-    private ?TwigEnvironment $view = null;
+    private ?TwigEnvironment $_view = null;
 
     public function initilize()
     {
         // TODO: proper error handling
         $loader     = new TwigFilesystemLoader($this->route_data['views']['views_dir']);
-        $this->view = new TwigEnvironment($loader, [
+        $this->_view = new TwigEnvironment($loader, [
             'cache' => $this->route_data['views']['cache_dir'],
         ]);
     }
 
+    /** It returns a single database manager instance. */
+    public function db(): Capsule
+    {
+        // TODO: proper error handling
+        return Database::conn($this->route_data['database']);
+    }
+
     public function render(string $view_file, array $data = []): string
     {
-        if ($this->view === null) {
+        if ($this->_view === null) {
             $this->initilize();
         }
 
-        return $this->view->render($view_file, $data);
+        return $this->_view->render($view_file, $data);
     }
 }
---
+++ src/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Leap;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+/** `Leap` database class. */
+final class Database
+{
+    private static ?Capsule $capsule = null;
+
+    /** Get an application database instance. */
+    public static function conn(array $config): Capsule
+    {
+        if (self::$capsule == null) {
+            self::$capsule = new Capsule;
+            self::$capsule->addConnection($config);
+            self::$capsule->setAsGlobal();
+            self::$capsule->bootEloquent();
+        }
+        return self::$capsule;
+    }
+}
